361b12b5cb2a8baee41cfdf67e012ca98435ba08
Arpit Tyagi <arpittyagi102@gmail.com>
2023-07-20 19:56:12 +0530

    initial animation done

--- src/app/components/Welcome.jsx
+++ src/app/components/Welcome.jsx
@@ -2,43 +2,60 @@
 
 import { welcomeSvgPath,introSvgPath } from '../Assets/welcomeSvgPath';
 import { motion } from 'framer-motion';
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
 
 export default function Welcome(){
 
     const [svgPath, setSvgPath] = useState(true);
+    const [remove, setRemove] = useState(false);
     
     useEffect(()=>{
-        const interval = setInterval(()=>{
-            setSvgPath(pres => !pres);
-        },9000)
+        setTimeout(() => {
+          setSvgPath(pres => !pres);
+        }, 4000);
 
-        return () => clearInterval(interval)
-    })
-
-    useEffect(()=>{
-        console.log("changed")
-    },[svgPath])
+        setTimeout(() => {
+          setRemove(true);
+        }, 8000);
+    },[])
     
-  return (
-      <div className='bg-red'>
-      <svg xmlns="http://www.w3.org/2000/svg"  width="883" height="102" >
-        <motion.path
-          d={svgPath ? welcomeSvgPath : introSvgPath}
-          fill="transparent"
-          strokeWidth="1"
-          stroke="rgba(255, 255, 255, 0.69)"
-          initial={{ pathLength: 0 }}
-          animate={{ pathLength: 1 }}
-          transition={{
-            duration: 7,
-            ease: "easeInOut",
-            repeat: Infinity,
-            repeatType: "loop",
-            repeatDelay: 2,
-          }}
-        /> 
-      </svg>
-      </div>
+    return (
+      (
+      !remove
+      ? <div>
+              {svgPath ? (
+              <svg xmlns='http://www.w3.org/2000/svg' width='883' height='102'>
+                <motion.path
+                  d={welcomeSvgPath}
+                  fill='transparent'
+                  strokeWidth='1'
+                  stroke='rgba(255, 255, 255, 0.69)'
+                  initial={{ pathLength: 0 }}
+                  animate={{ pathLength: 1 }}
+                  transition={{
+                    duration: 7,
+                    ease: 'easeInOut',
+                  }}
+                />
+              </svg>
+            ) : (
+              <motion.svg xmlns='http://www.w3.org/2000/svg' width='660' height='102'>
+                <motion.path
+                  d={introSvgPath}
+                  fill='transparent'
+                  strokeWidth='1'
+                  stroke='rgba(255, 255, 255, 0.69)'
+                  initial={{ pathLength: 0 }}
+                  animate={{ pathLength: 1 }}
+                  transition={{
+                    duration: 7,
+                    ease: 'easeInOut',
+                  }}
+                />
+              </motion.svg>
+            )}
+            </div>
+      : null
+      )
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,17 @@
 'use client'
 
 import Welcome from "./components/Welcome";
+import { motion } from "framer-motion";
 
 export default function Home() {
-  
+
   return (
     <main className="flex min-h-screen bg-black items-center justify-center">
-      <Welcome/>
+      
+        <Welcome/>
+
+      
+
     </main>
   );
 }
