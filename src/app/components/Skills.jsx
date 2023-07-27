0297aae74ce2da063e5977464587ceecc3a01046
Arpit Tyagi <arpittyagi102@gmail.com>
2023-07-27 12:55:10 +0530

    creating skills page

---
+++ src/app/components/Skills.jsx
@@ -0,0 +1,31 @@
+import react, { useState } from 'react';
+import cloudsBg from '../Assets/sky-clouds-bg.svg';
+import hotAirBallon from '../Assets/hot-air-balloon.svg';
+import { motion } from 'framer-motion';
+
+export default function Skills(){
+
+    const [value, setValue] = useState(true)
+
+    const string = ['C','C++','HTML','CSS','Javascript','Jquery','Bootstrap','React.js','Redux','Next.js','Tailwind CSS','FramerMotion','Node.js','Express','MySQL','MongoDB','Git','Postman']
+    return(
+        <div className={`w-full flex justify-evenly items-center flex-wrap ${value ? "flex-row-reverse" : ""}`} style={{backgroundImage:`url(${cloudsBg.src})`}}>
+            <button onClick={() => setValue((item) => !item)}> CLick me</button>
+            {
+                string.map((item)=>{
+                    return <Balloon value={item} />
+                })
+            }
+        </div>
+    )
+}
+
+function Balloon({value}){
+    return(
+        <motion.div className="w-40 h-40 bg-contain flex justify-center items-center bg-no-repeat m-10" style={{backgroundImage:`url(${hotAirBallon.src})`}} drag layout>
+            <div className='font-extrabold text-green-800 border p-2 border-green-800 rounded-full' style={{translate:'0 -30px'}}>
+                {value}
+            </div>
+        </motion.div>
+    )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,16 @@
 'use client'
 
 //import Welcome from "./components/Welcome";
-//import Home from "./components/Home"
+import Home from "./components/Home"
 import About from "./components/About";
+import Skills from "./components/Skills";
 import { motion,useScroll,useTransform } from "framer-motion";
 
 export default function App() {
 
   return (
     <main className="flex min-h-screen bg-black justify-center">
-        <About/>        
+        <Skills/>        
 
     </main>
   );
