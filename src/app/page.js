aa536e6320c259e1280bcf49ad3b0a727397b811
Arpit Tyagi <arpittyagi102@gmail.com>
2023-08-08 14:51:13 +0530

    creating projects page

--- src/app/page.js
+++ src/app/page.js
@@ -3,14 +3,15 @@
 //import Welcome from "./components/Welcome";
 //import Home from "./components/Home"
 //import About from "./components/About";
-import Skills from "./components/Skills";
+import Projects from './components/Projects';
+//import Skills from "./components/Skills";
 import { motion,useScroll,useTransform } from "framer-motion";
 
 export default function App() {
 
   return (
     <main className="flex min-h-screen bg-black justify-center">
-        <Skills/>        
+        <Projects/>        
 
     </main>
   );
