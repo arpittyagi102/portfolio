0ec7fc01f43ccc98926f7084a09c286e101da380
Arpit Tyagi <arpittyagi102@gmail.com>
2023-08-21 17:52:29 +0530

    added forks button

--- src/app/components/Project.jsx
+++ src/app/components/Project.jsx
@@ -6,20 +6,25 @@ import Image from 'next/image';
 
 export default function Project({ projectData }){
 
+    const [pn, setpn] = useState(0);
+    const [stargazers, setStargazers] = useState(0);
+    const [forks, setForks] = useState(0);
+
     useEffect(() => {
-        fetch('https://api.github.com/repos/arpittyagi102/LinkUp/stargazers')
+        fetch(`https://api.github.com/repos/arpittyagi102/${projectData[pn].urlName}`)
             .then(response => response.json())
-            .then(data => setStargazers(data.length))
+            .then(data => {setStargazers(data.stargazers_count); setForks(data.forks_count)})
             .catch(error => console.error("Error:", error));
-    });
+    },[pn]);
 
     const [showLowerStats, setShowLowerStats] = useState(false);
     
     useEffect(()=>{
+        setShowLowerStats(false);
         setTimeout(() => {
             setShowLowerStats(true);
         }, 10000);
-    })
+    },[pn])
 
 
     const container = {
@@ -35,8 +40,6 @@ export default function Project({ projectData }){
         hidden: { opacity: 0 }
     };
 
-    const [pn, setpn] = useState(0);
-    const [stargazers, setStargazers] = useState(0);
 
     function changeProject(){
         if(pn+1 != projectData.length)
@@ -47,22 +50,24 @@ export default function Project({ projectData }){
     return (
     <>  <motion.div  variants={container} initial="hidden" animate="visible" exit={{opacity:0}} key={projectData[pn].title} className='w-2/5'>
             <h1 className='text-black text-5xl' style={{ fontFamily: 'borel',maxWidth:'100%'}} onClick={changeProject}>{projectData[pn].title}</h1>
-            <p className='text-black font-bold w-full'>
+            <p className='text-black font-bold w-full '>
                 {projectData[pn].summary.split('').map((item,key) => {
                     return(
                         <motion.span key={key} variants={child} className="text-xl font-mono font-bold">{item}</motion.span>
                     )}
                 )}              
             </p>
-            {showLowerStats &&
-                <motion.div className="flex">
+                <motion.div className={`flex ${!showLowerStats && 'hidden'}`}>
                     <h1 className="m-5 p-3 px-6 rounded-full bg-slate-900 flex text-lg font-bold">
                         {stargazers} 
                         <img height="20px" src="https://img.icons8.com/fluency/24/star.png" className="ms-2" alt="star"/></h1>
-                    <h1 className="m-5 p-3 px-6 rounded-full bg-slate-900">
-                        {stargazers}</h1>
+                    <h1 className="m-5 p-3 px-6 rounded-full bg-slate-900 flex text-lg font-bold">
+                        {forks}
+                        <img src="https://img.icons8.com/ffffff/ios-glyphs/30/code-fork.png" className="ms-2" alt="code-fork"/></h1>
+                    <a className="m-5 p-3 px-6 rounded-full bg-slate-900 flex text-lg font-bold" href={`https://github.com/arpittyagi102/${projectData[pn].urlName}`}>
+                        Github
+                        <img src="https://img.icons8.com/ffffff/windows/30/external-link.png" className="ms-2" alt="externals link"/></a>
                 </motion.div>
-            }
             
         </motion.div>
         <motion.div 
--- src/app/components/Projects.jsx
+++ src/app/components/Projects.jsx
@@ -6,7 +6,7 @@ export default function Projects(){
 
     const projectsData=[
         {
-            urlName:'HumariDukan',
+            urlName:'Humari-dukan',
             title:'Humari Dukan',
             summary:'Humari Dukaan is a fully functional ecommerce website built using React.js, Redux, React Router, Bootstrap, and Bootstrap Icons. The project aims to provide users with a seamless shopping experience and incorporates various features such as image carousel, scroll animations, search functionality, category-wise shopping, and a robust checkout system.',
             link:'https://humaridukan.netlify.app/'
@@ -17,6 +17,18 @@ export default function Projects(){
             summary:' This is a MERN stack project that provides an attractive and seamless chatting experience. With its user-friendly interface and advanced features, this application allows users to engage in real-time conversations with others, keeping them connected and up to date.',
             link:'https://getlinkup.vercel.app/'
         },
+        {
+            urlName:'HideandSeek',
+            title:'Hide and Seek',
+            summary:'Welcome to Hide and Seek, a web game built using vanilla JavaScript. In this game you are challenged to find hidden gifts and avoid the dangerous bomb. Test your luck and play this simple and fun game, You can play a hide and seek game in it with 3 levels and 3 themes',
+            link:'https://hideandseekk.vercel.app'
+        },
+        {
+            urlName:'speedykeys',
+            title:'Speedy keys',
+            summary:'Welcome to Speedy Keys, a web-based typing speed test application that allows you to practice and improve your typing skills. Test your typing speed, accuracy, and efficiency with different difficulty levels and time durations',
+            link:'https://speedykeys.vercel.app'
+        }
     ]
 
     
